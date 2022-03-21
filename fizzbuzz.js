f7baee66506c6dae0d42e8cc7ca6b3ab5ce7e921
AJ <70864447+andrewjamesmoore@users.noreply.github.com>
2022-03-20 23:29:52 -0600

    Update with refactor in notes

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -2,6 +2,9 @@
 
 /*
 print numbers from 1 to 100. Multiples of three should print “Fizz”; multiples of 5 should print "Buzz"; multiples of 3 and 5 should print “FizzBuzz”.
+
+for (let i = 1; i < 101;) console.log((i++ % 3 ? '' : 'fizz') + (i % 5 ? '' : 'buzz') || i)
+
 */
 
 for (let n = 1; n < 101; n++) {
