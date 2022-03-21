867382ce5e19c4431e385c9ccd87994a94646aa7
Andrew Moore <hello@andrewjamesmoore.com>
2022-03-20 22:14:03 -0600

    Solution

--- loopingatriangle.js
+++ loopingatriangle.js
@@ -1,1 +1,15 @@
+/* 
+write a loop to console.log() to output the following triangle:
+#
+##
+###
+####
+#####
+######
+#######
+*/
+"use strict";
 
+for (let brick = "#"; brick.length < 8; brick += "#") {
+  console.log(brick);
+}
